b81c8519d84ce765e0fac3bd78ec4a021c02d76d
Js-cook <jscook1001@gmail.com>
2025-01-13 14:12:24 -0500

    Added basic layout of main page

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>ERG Sprints Spreadsheet Utility</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <form>
+          <label for="file-input">Input File: </label>
+          <input type="file" id="file-input" />
+        </form>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        NOTE: Program assumes correctly formatted input spreadsheet.
       </p>
     </>
   )
